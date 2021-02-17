9440f32ad3336a720be4d7011dd346eef4fe1211
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-02-17 15:25:38 +0800

    Started learning about asynchronous programming

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,15 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  //Function that gets a devices location
+  void getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  //Async Programming allows functions to run in the background
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +23,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
